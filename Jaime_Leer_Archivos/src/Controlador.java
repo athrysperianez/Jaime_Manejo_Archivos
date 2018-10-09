0ed8b830f6c00850dfc879ad9e647b590ee1930c
Elias Periañez <eliasperianez@gmail.com>
2018-10-09 12:09:49 +0200

    Añadido comentarios

--- Jaime_Leer_Archivos/src/Controlador.java
+++ Jaime_Leer_Archivos/src/Controlador.java
@@ -2,6 +2,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
+/*
+ *Creado por Elias Periañez
+ *8 oct. 2018
+ *Como parte del proyecto Jaime_Leer_Archivos
+ *Este archivo esta bajo la licencia de Creative Commons Reconocimiento 4.0 Internacional (Más informacion https://creativecommons.org/licenses/by/4.0/)
+________________________________________________________________________________________________________________________________________________________
+ *Created by Elias Periañez
+ *8 oct. 2018
+ *As part of the project Jaime_Leer_Archivos
+ *This file is under the Creative Commons Attribution 4.0 International (More info here https://creativecommons.org/licenses/by/4.0/)
+ */
+
 public class Controlador {
 
 	private Modelo md;
